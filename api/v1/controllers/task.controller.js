70ce23ba06ed29264fdf7fab1ede22993f4601fa
Nvhanh <nvhanh263@gmail.com>
2024-07-06 17:29:38 +0700

    filter by status

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -1,16 +1,29 @@
 const Task = require("../models/task.model");
 
 // [GET] /api/v1/task
-module.exports.getTasks = async (req, res) => {
-    const tasks = await Task.find({
+module.exports.index = async (req, res) => {
+    const filter = {
         deleted: false
-    });
+    }
+
+    // filter by status
+    const status = req.query.status;
+    const listStatus = ["initial", "doing", "finish", "pending", "notFinish"];
+    if (listStatus.includes(status)) {
+        filter.status = status;
+    } else {
+        return res.sendStatus(400);
+    }
+    // end filter by status
+
+
+    const tasks = await Task.find(filter);
 
     return res.status(200).json(tasks);
 }
 
 // [GET] /api/v1/task/detail/:id
-module.exports.getTaskDetail = async (req, res) => {
+module.exports.detail = async (req, res) => {
     try {
         const taskId = req.params.id;
         const task = await Task.findOne({
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const controller = require("../controllers/task.controller");
 
-router.get("/", controller.getTasks);
+router.get("/", controller.index);
 
-router.get("/detail/:id", controller.getTaskDetail);
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
