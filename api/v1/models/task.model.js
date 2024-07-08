050c31d1bccd54ebc8d91de34bbf499b4d677a15
Nvhanh <nvhanh263@gmail.com>
2024-07-08 16:05:04 +0700

    save id of user created task

--- api/v1/models/task.model.js
+++ api/v1/models/task.model.js
@@ -1,17 +1,15 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema(
     {
-        title: String,
-        status: String,
-        content: String,
-        timeStart: Date,
-        timeFinish: Date,
-        deleted: {
-            type: Boolean,
-            default: false
-        },
-        deletedAt: Date
+        title: { type: String, required: true },
+        status: { type: String, required: true },
+        content: { type: String, required: true },
+        timeStart: { type: Date, required: true },
+        timeFinish: { type: Date, required: true },
+        createBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        deleted: { type: Boolean, default: false },
+        deletedAt: { type: Date },
     },
     { timestamps: true }
 );
--- api/v1/models/user.model.js
+++ api/v1/models/user.model.js
@@ -2,20 +2,15 @@ const { default: mongoose } = require("mongoose");
 
 const userSchema = new mongoose.Schema(
     {
-        fullName: String,
-        email: String,
-        password: String,
-        deleted: {
-            type: Boolean,
-            default: false
-        }
+        fullName: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        deleted: { type: Boolean, default: false }
         // phone
         // avatar
         // status
     },
-    {
-        timestamps: true
-    }
+    { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema, "users");
